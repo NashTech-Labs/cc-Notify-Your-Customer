39dd9891e90511882871a71ef71ff49f8ee58d9f
Harshit Daga <harshit@knoldus.com>
2017-06-21 17:12:47 +0530

    Updated project structure

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.{Def, _}
 import sbt.Keys._
 
 object ProjectSettings {
@@ -6,17 +6,26 @@ object ProjectSettings {
   def BaseProject(name: String): Project = Project(name, file(name))
     .settings(
       Defaults.coreDefaultSettings ++
-      Seq(
-      organization                          := "com.knoldus",
-      scalaVersion in ThisBuild             := "2.12.0",
-      version 			                        := "1.0.0",
-//      scapegoatVersion                      := "1.1.0",
-      fork in Test 			                    := true,
-      fork in IntegrationTest 			        := true,
-      parallelExecution in IntegrationTest 	:= false,
-      parallelExecution in Test 	          := false,
-//      test in assembly                      := {},
-      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-      resolvers += "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases/"
-    ))
+        Seq(
+          organization := "com.knoldus",
+          scalaVersion in ThisBuild := "2.12.0",
+          version := "1.0.0",
+            fork in Test := true,
+          fork in IntegrationTest := true,
+          parallelExecution in IntegrationTest := false,
+          parallelExecution in Test := false,
+          scalaSource in Compile := baseDirectory.value / "src",
+          resourceDirectory in Compile := baseDirectory.value / "resources",
+          projectResolvers
+        ))
+
+  def projectResolvers: Def.Setting[Seq[Resolver]] = {
+    resolvers ++= Seq(
+      Resolver.sonatypeRepo("releases"),
+      "Twitter Maven" at "https://maven.twttr.com",
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases/",
+      "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+    )
+  }
 }

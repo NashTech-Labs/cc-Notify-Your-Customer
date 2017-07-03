c161a7f7d578ee4e3074e6158ef253f7a5197018
Girish Bharti <girish@knoldus.com>
2017-07-03 21:54:31 +0530

    Clean up

--- common-util/src/main/scala/com/ping/date/DateUtil.scala
+++ common-util/src/main/scala/com/ping/date/DateUtil.scala
@@ -2,36 +2,15 @@ package com.ping.date
 
 import java.sql.Timestamp
 
-import org.joda.time.{DateTime, DateTimeZone}
-import org.joda.time.format.DateTimeFormat
-
 trait DateUtil {
-
-  def currentTime: Long = System.currentTimeMillis
 
   def currentTimestamp: Timestamp = new Timestamp(currentTime)
 
-  private val dateFormats: Seq[String] = Seq("yyyy-MM-dd HH:mm", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
-    "yyyy-MM-dd'T'HH:mm:ssZZ", "yyyy-MM-dd'T'HH:mm:ss.SSS", "yyyy-MM-dd'T'HH:mm:ss-SSSS", "yyyy-MM-dd'T'HH:mm:ss+SSSS", "yyyy-MM-dd'T'HH:mmZ")
-
-  def getDateTime(dateString: String): DateTime = {
-    DateTimeZone.setDefault(DateTimeZone.UTC)
-    def getDateTime(dateFormats: Seq[String], dateString: String): DateTime = {
-      try {
-        val dtFormat = DateTimeFormat.forPattern(dateFormats.head)
-        dtFormat.parseDateTime(dateString)
-      } catch {
-        case _ if dateFormats.nonEmpty => getDateTime(dateFormats.tail, dateString)
-        case ex: Exception =>
-          throw new IllegalArgumentException("Unknown date format -> " + dateString, ex)
-      }
-    }
-    getDateTime(dateFormats, dateString)
-  }
+  def currentTime: Long = System.currentTimeMillis
 
 }
 
-trait UUIDHelper{
+trait UUIDHelper {
 
   def getRandomUUID: String = java.util.UUID.randomUUID().toString
 
--- common-util/src/main/scala/com/ping/json/JsonHelper.scala
+++ common-util/src/main/scala/com/ping/json/JsonHelper.scala
@@ -15,31 +15,18 @@ import scala.language.implicitConversions
 
 trait JsonHelper{
 
-  val EMPTY_STRING = ""
-  val JavaNull = null
-
+  val empty = ""
+  val javaNull = null
 
   implicit val formats = new org.json4s.DefaultFormats {
     override def dateFormatter: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
-  } ++ org.json4s.ext.JodaTimeSerializers.all ++ List(IntToDateTimeSerializer, StringToBigDecimalSerializer)
-
-  case object IntToDateTimeSerializer extends CustomSerializer[DateTime](format => (
-    {
-      case JInt(value) => new DateTime(value.longValue)
-      case JLong(value) => new DateTime(value)
-      case JNull => JavaNull
-      case JString(value) => DateUtil.getDateTime(value)
-    },
-    {
-      case d: DateTime => JString(d.toString)
-    }
-  ))
+  } ++ org.json4s.ext.JodaTimeSerializers.all ++ List(StringToBigDecimalSerializer)
 
   case object StringToBigDecimalSerializer extends CustomSerializer[BigDecimal](format => (
     {
       case JInt(value) => BigDecimal(value.longValue)
       case JLong(value) => BigDecimal(value)
-      case JNull => JavaNull
+      case JNull => javaNull
       case JString(value) => Try(BigDecimal(value.toInt)).getOrElse(BigDecimal(0))
     },
     {
@@ -54,7 +41,7 @@ trait JsonHelper{
   protected def pretty(value: String): String = jPretty(render(parse(value)))
 
   implicit protected def extractOrEmptyString(json: JValue): String = json match {
-    case JNothing => EMPTY_STRING
+    case JNothing => empty
     case data     => data.extract[String]
   }
 
